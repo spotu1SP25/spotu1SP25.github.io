772b57b28b64c033c0a5b941f8f02559e2be17e1
Sushant Potu <spotu1@my.waketech.edu>
2025-07-14 16:20:30 -0400

    lesson 12

---
+++ gradebook.js
@@ -0,0 +1,11 @@
+
+function fetchGradeData() {
+    console.log("Fetching grade data...");
+}
+
+function populateGradebook(data) {
+    console.log("Populating gradebook with data:", data);
+}
+
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
